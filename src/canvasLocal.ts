a04ad7aaf76a54d9278be0628d960e089d534db8
VictorDavidVasquezLopez <vicktorvas94@gmail.com>
2021-09-26 17:21:37 -0500

    dibujar triangulo malla

--- src/canvasLocal.ts
+++ src/canvasLocal.ts
@@ -2,13 +2,14 @@
 export class CanvasLocal {
   //atributos
   protected graphics: CanvasRenderingContext2D;
-  rWidth:number =5;
-  rHeight:number = 5;
-   maxX: number;
-   maxY: number;
-   pixelSize: number;
-   centerX: number;
-   centerY: number;
+  protected rWidth:number;
+  protected rHeight:number;
+  protected maxX: number;
+  protected maxY: number;
+  protected pixelSize: number;
+  protected centerX: number;
+  protected centerY: number;
+  
 
   
    //pixelWidth: number;
@@ -17,8 +18,8 @@ export class CanvasLocal {
       
   public constructor(g: CanvasRenderingContext2D, canvas: HTMLCanvasElement){
      this.graphics = g;
-     this.rWidth=640; //le agregue
-     this.rHeight=480; //le agregue
+     this.rWidth=10; //le agregue
+     this.rHeight=10; //le agregue
     //this.rWidth = 6;
     //this.rHeight= 4;
      this.maxX = canvas.width - 1
@@ -36,8 +37,8 @@ export class CanvasLocal {
     //this.centerY = this.maxY/2;
   }
  //para hacer un modo anisotropico
- iX( x: number):number { return Math.round(this.centerX - x  / this.pixelSize) ; }
- iY( y: number): number { return this.maxY - Math.round (this.centerY + y / this.pixelSize);}
+ iX( x: number):number { return Math.round(this.centerX + x  / this.pixelSize) ; }
+ iY( y: number): number { return this.maxY - Math.round (this.centerY - y / this.pixelSize);}
  
  
  //dibuja las lineas 
@@ -57,7 +58,36 @@ paint() {
   this.drawLine(320, 40, 140, 400);//le agregue   120, 30, 40,300
   this.drawLine(140, 400, 480, 400)// le agregue  40,300,380,300
   //this.drawLine(320,40,420,400);//le agregue
- 
+  let side= 0.9 *480;
+  let sideHalf= 0.5 * side;
+  let xCenter= 320;
+  let yCenter= 240;
+
+  let h= sideHalf * Math.sqrt(3);
+  let xA, yA, xB, yB, xC, yC, xA1, yA1, xB1, yB1, xC1, yC1, p, q;
+  q= 0.05;
+  p= 1-q;
+  xA= xCenter - sideHalf;
+  yA=yCenter -0.5 * h;
+  xB= xCenter + sideHalf;
+  yB=yA;
+  xC= xCenter;
+  yC=yCenter + 0.5 * h;
+  for (let i=0;i<50;i++)
+  {
+    this.drawLine(xA, yA, xB,  yB);
+    this.drawLine(xB, yB, xC,  yC);
+    this.drawLine(xC, yC, xA,  yA);
+    xA1 = p * xA + q * xB;
+    yA1 = p * yA + q * yB;
+    xB1 = p * xB + q * xC;
+    yB1 = p * yB + q * yC;
+    xC1 = p * xC + q * xA;
+    yC1 = p * yC + q * yA;
+    xA = xA1; xB = xB1; xC = xC1;
+    yA = yA1; yB = yB1; yC = yC1;
+
+  }
   /*this.drawLine(this.iX(0), this.iY(0), this.iX(1), this.iY(0));
   this.drawLine(this.iX(1), this.iY(0), this.iX(1), this.iY(1));
   this.drawLine(this.iX(1), this.iY(1), this.iX(0), this.iY(1));
